6a20d715e20a2202195f79286f1fa7e66e563104
Esther Weon <esther.s.weon@gmail.com>
2017-09-05 11:20:13 -0700

    Step 3: Database Setup

--- models/album.js
+++ models/album.js
@@ -2,3 +2,16 @@
 // set up shorthand Schema variable to stand in for mongoose.Schema
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+// album schema
+var AlbumSchema = new Schema({
+  artistName: String,
+  name: String,
+  releaseDate: String,
+  genres: [ String ]
+});
+
+// album model
+var Album = mongoose.model('Album', AlbumSchema);
+
+module.exports = Album;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
 // require mongoose and connect to database
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/tunely_test');
+
+var Album = require('./album');
+
+module.exports = {
+  Album: Album
+};
