752733870d6327d8ef30843f63bbe9f92f73621c
Esther Weon <esther.s.weon@gmail.com>
2017-09-05 11:22:04 -0700

    Step 4: Seeding the database

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -1,29 +1,3 @@
-var albums = [{
-  _id: 132,
-  artistName: 'Nine Inch Nails',
-  name: 'The Downward Spiral',
-  releaseDate: '1994, March 8',
-  genres: [ 'industrial', 'industrial metal' ]
-}, {
-  _id: 133,
-  artistName: 'Metallica',
-  name: 'Metallica',
-  releaseDate: '1991, August 12',
-  genres: [ 'heavy metal' ]
-}, {
-  _id: 134,
-  artistName: 'The Prodigy',
-  name: 'Music for the Jilted Generation',
-  releaseDate: '1994, July 4',
-  genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
-}, {
-  _id: 135,
-  artistName: 'Johnny Cash',
-  name: 'Unchained',
-  releaseDate: '1996, November 5',
-  genres: [ 'country', 'rock' ]
-}];
-
 // GET /api/albums
 function index(req, res) {
   res.json(albums);
--- seed.js
+++ seed.js
@@ -1,8 +1,26 @@
 var db = require("./models");
 
-var albumsList =[
-  // data here soon!
-];
+var albumsList = [{
+  artistName: 'Nine Inch Nails',
+  name: 'The Downward Spiral',
+  releaseDate: '1994, March 8',
+  genres: [ 'industrial', 'industrial metal' ]
+}, {
+  artistName: 'Metallica',
+  name: 'Metallica',
+  releaseDate: '1991, August 12',
+  genres: [ 'heavy metal' ]
+}, {
+  artistName: 'The Prodigy',
+  name: 'Music for the Jilted Generation',
+  releaseDate: '1994, July 4',
+  genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+}, {
+  artistName: 'Johnny Cash',
+  name: 'Unchained',
+  releaseDate: '1996, November 5',
+  genres: [ 'country', 'rock' ]
+}];
 
 db.Album.remove({}, function(err, albums){
   // code in here runs after all albums are removed
