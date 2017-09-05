2077bffb509eb7da287f7090b25f6cbf16d6066b
Esther Weon <esther.s.weon@gmail.com>
2017-09-05 12:22:47 -0700

    Define array in more intuitive way

--- docs/code_samples/sprint3_song_seeds.js
+++ docs/code_samples/sprint3_song_seeds.js
@@ -1,32 +1,32 @@
-/*  Approximate schema for these seeds
+/*  Schema for Song seeds
 
 var SongSchema = new Schema({
   name: String,
   trackNumber: Number,
 });
+
 */
 
 
-var sampleSongs = [];
-
-sampleSongs.push({ name: 'Swamped',
-                   trackNumber: 1
-});
-sampleSongs.push({ name: "Heaven's a Lie",
-                   trackNumber: 2
-});
-sampleSongs.push({ name: 'Daylight Dancer',
-                   trackNumber: 3
-});
-sampleSongs.push({ name: 'Humane',
-                   trackNumber: 4
-});
-sampleSongs.push({ name: 'Self Deception',
-                   trackNumber: 5
-});
-sampleSongs.push({ name: 'Aeon',
-                   trackNumber: 6
-});
-sampleSongs.push({ name: 'Tight Rope',
-                   trackNumber: 7
-});
+var songsList = [{ 
+  name: 'Swamped',
+  trackNumber: 1
+}, { 
+  name: "Heaven's a Lie",
+  trackNumber: 2
+}, { 
+  name: 'Daylight Dancer',
+  trackNumber: 3
+}, { 
+  name: 'Humane',
+  trackNumber: 4
+}, { 
+  name: 'Self Deception',
+  trackNumber: 5
+}, { 
+  name: 'Aeon',
+  trackNumber: 6
+}, { 
+  name: 'Tight Rope',
+  trackNumber: 7
+}];
