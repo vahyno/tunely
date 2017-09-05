9fe41ee153e5dd94172bcfe8ea9ceeae9b90e9d3
Esther Weon <esther.s.weon@gmail.com>
2017-09-05 10:54:06 -0700

    Clean up app.js to define hard-coded data more intuitively

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,37 +4,30 @@
  * into functions and objects as needed.
  *
  */
+ 
 
-
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
-
-
+// hard-coded data
+var sampleAlbums = [{
+  artistName: 'Ladyhawke',
+  name: 'Ladyhawke',
+  releaseDate: '2008, November 18',
+  genres: [ 'new wave', 'indie rock', 'synth pop' ]
+}, {
+  artistName: 'The Knife',
+  name: 'Silent Shout',
+  releaseDate: '2006, February 17',
+  genres: [ 'synth pop', 'electronica', 'experimental' ]
+}, {
+  artistName: 'Juno Reactor',
+  name: 'Shango',
+  releaseDate: '2000, October 9',
+  genres: [ 'electronic', 'goa trance', 'tribal house' ]
+}, {
+  artistName: 'Philip Wesley',
+  name: 'Dark Night of the Soul',
+  releaseDate: '2008, September 12',
+  genres: [ 'piano' ]
+}];
 
 
 $(document).ready(function() {
@@ -42,11 +35,7 @@ $(document).ready(function() {
 });
 
 
-
-
-
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
   console.log('rendering album:', album);
-
 }
