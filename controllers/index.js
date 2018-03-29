acaa6469fdee4408e97d776aedd45959d3dc88ad
vahyno <vahyno@gmail.com>
2018-03-29 16:32:32 -0700

    completed sprint 0, basic server and database set up

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+
+
+module.exports = {                    //in object to allow export of multiple controllers
+  api: require('./apiController'),
+  albums: require('./albumsController')
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser'); // do we need this?
+var db = require('./models');
+var controllers = require('./controllers');
+
+app.use(express.static('public'));
+
+app.get('/',function(req, res){
+  res.sendFile('views/index.html', {root : __dirname});
+});
+
+app.get('/api', controllers.api.index);   // brings me all the way to controllers=>index.js=>apiController.js
+
+
+app.listen(3000);
